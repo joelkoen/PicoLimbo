73b5cfe2c2ae0a4e1d43ec01d52a2ee3809d877c
Quozul <30729291+Quozul@users.noreply.github.com>
2025-02-06 17:27:59 +0100

    feat: packet handler (wip)

--- server/src/state/handle_status_state.rs
+++ server/src/state/handle_status_state.rs
@@ -1,7 +1,6 @@
 use crate::packet_error::PacketError;
 use crate::packets::status::ping_request_packet::PingRequestPacket;
 use crate::packets::status::status_request_packet::StatusRequestPacket;
-use crate::state::State;
 use protocol::prelude::{DecodePacket, PacketId};
 
 pub enum StatusResult {
@@ -10,6 +9,14 @@ pub enum StatusResult {
 }
 
 pub fn handle_status_state(packet_id: u8, payload: &[u8]) -> Result<StatusResult, PacketError> {
+    let handler = PacketHandler::new()
+        .on::<StatusRequestPacket>(|packet| StatusResult::Status)
+        .on::<PingRequestPacket>(|packet| StatusResult::Ping(packet.timestamp));
+    handler.handle(packet_id, payload)
+
+    /*
+    // This is the previous code, that manually parses each packet with a match
+
     match packet_id {
         StatusRequestPacket::PACKET_ID => {
             StatusRequestPacket::decode(payload)?;
@@ -20,5 +27,26 @@ pub fn handle_status_state(packet_id: u8, payload: &[u8]) -> Result<StatusResult
             Ok(StatusResult::Ping(packet.timestamp))
         }
         _ => Err(PacketError::new(State::Status, packet_id)),
+    }
+     */
+}
+
+pub struct PacketHandler<R> {}
+
+impl<Output> PacketHandler<Output> {
+    pub fn new() -> Self {
+        Self {}
+    }
+
+    pub fn on<T, F>(&mut self, func: F) -> Self
+    where
+        T: PacketId + DecodePacket,
+        F: Fn(T) -> Output + Clone,
+    {
+        todo!()
+    }
+
+    pub fn handle(&self, packet_id: u8, payload: &[u8]) -> Result<Output, PacketError> {
+        todo!()
     }
 }
