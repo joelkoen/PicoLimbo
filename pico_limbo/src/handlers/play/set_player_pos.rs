6858c417e1bba0f4ad673c3949ac6c89a0dcc4da
Axeno <tbavart@gmail.com>
2025-09-06 17:07:51 +0200

    Add set_player_pos module to handle player position updates and teleportation

---
+++ pico_limbo/src/handlers/play/set_player_pos.rs
@@ -0,0 +1,20 @@
+use minecraft_packets::play::set_player_position::SetPlayerPositionPacket;
+use crate::handlers::play::set_player_position_and_rotation::teleport_player_to_spawn;
+use crate::server::client_state::ClientState;
+use crate::server::packet_handler::{PacketHandler, PacketHandlerError};
+use crate::server_state::ServerState;
+
+impl PacketHandler for SetPlayerPositionPacket {
+    fn handle(
+        &self,
+        client_state: &mut ClientState,
+        server_state: &ServerState
+    ) -> Result<(), PacketHandlerError> {
+        let min_y_pos_config = server_state.min_y_pos();
+        if self.feet_y < min_y_pos_config as f64 {
+            teleport_player_to_spawn(client_state, server_state);
+        }
+
+        Ok(())
+    }
+}
--- pico_limbo/src/handlers/play/set_player_position_and_rotation.rs
+++ pico_limbo/src/handlers/play/set_player_position_and_rotation.rs
@@ -1,19 +1,33 @@
 use minecraft_packets::play::set_player_position_and_rotation::SetPlayerPositionAndRotationPacket;
+use minecraft_packets::play::synchronize_player_position_packet::SynchronizePlayerPositionPacket;
 use crate::server::client_state::ClientState;
 use crate::server::packet_handler::{PacketHandler, PacketHandlerError};
+use crate::server::packet_registry::PacketRegistry;
 use crate::server_state::ServerState;
 
 impl PacketHandler for SetPlayerPositionAndRotationPacket {
     fn handle(
         &self,
-        _client_state: &mut ClientState,
-        _server_state: &ServerState,
+        client_state: &mut ClientState,
+        server_state: &ServerState,
     ) -> Result<(), PacketHandlerError> {
-        println!(
-            "SetPlayerPositionAndRotationPacket received: x={}, y={}, z={}, yaw={}, pitch={}",
-            self.x, self.feet_y, self.z, self.yaw, self.pitch
-        );
+        let min_y_pos_config = server_state.min_y_pos();
+        if self.feet_y < min_y_pos_config as f64 {
+            teleport_player_to_spawn(client_state, server_state);
+        }
 
         Ok(())
+    }
+}
+
+pub fn teleport_player_to_spawn(
+    client_state: &mut ClientState,
+    server_state: &ServerState,
+) {
+    let (x, y, z) = server_state.spawn_position();
+    let packet = SynchronizePlayerPositionPacket::new(x, y, z);
+    client_state.queue_packet(PacketRegistry::SynchronizePlayerPosition(packet));
+    if let Some(content) = server_state.min_y_message() {
+        client_state.send_message(content);
     }
 }
