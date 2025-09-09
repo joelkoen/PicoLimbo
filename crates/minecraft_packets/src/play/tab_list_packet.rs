2967feb65a537f5b5228b3ac98c7b09a79ffcdf7
Axeno <tbavart@gmail.com>
2025-09-09 21:42:27 +0200

    refactor: Update packet to support all mc version

--- crates/minecraft_packets/src/play/tab_list_packet.rs
+++ crates/minecraft_packets/src/play/tab_list_packet.rs
@@ -3,15 +3,23 @@ use pico_text_component::prelude::Component;
 
 #[derive(PacketOut)]
 pub struct TabListPacket {
-    pub header: Nbt,
-    pub footer: Nbt,
+    #[pvn(..765)]
+    header: String,
+    #[pvn(..765)]
+    footer: String,
+    #[pvn(765..)]
+    v1_20_3_header: Nbt,
+    #[pvn(765..)]
+    v1_20_3_footer: Nbt,
 }
 
 impl TabListPacket {
     pub fn new(content: &Component, overlay: &Component) -> Self {
         Self {
-            header: content.to_nbt(),
-            footer: overlay.to_nbt(),
+            header: content.to_json(),
+            footer: overlay.to_json(),
+            v1_20_3_header: content.to_nbt(),
+            v1_20_3_footer: overlay.to_nbt(),
         }
     }
 }
