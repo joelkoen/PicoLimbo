faf52bf47b73658564bd2f8ea060f9c3998975b9
Quozul <30729291+Quozul@users.noreply.github.com>
2025-09-10 10:01:00 +0200

    refactor: Implement EncodePacket for Component

--- crates/minecraft_packets/src/play/tab_list_packet.rs
+++ crates/minecraft_packets/src/play/tab_list_packet.rs
@@ -3,23 +3,15 @@ use pico_text_component::prelude::Component;
 
 #[derive(PacketOut)]
 pub struct TabListPacket {
-    #[pvn(..765)]
-    header: String,
-    #[pvn(..765)]
-    footer: String,
-    #[pvn(765..)]
-    v1_20_3_header: Nbt,
-    #[pvn(765..)]
-    v1_20_3_footer: Nbt,
+    header: Component,
+    footer: Component,
 }
 
 impl TabListPacket {
     pub fn new(content: &Component, overlay: &Component) -> Self {
         Self {
-            header: content.to_json(),
-            footer: overlay.to_json(),
-            v1_20_3_header: content.to_nbt(),
-            v1_20_3_footer: overlay.to_nbt(),
+            header: content.clone(),
+            footer: overlay.clone(),
         }
     }
 }
